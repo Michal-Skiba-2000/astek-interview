198f74fe4463139901e95815be3ed9b46dcdf01f
Michal-Skiba-2000 <michal.skiba.2000@gmail.com>
2022-02-24 01:01:57 +0100

    Changed to dishes be contained by menus

--- emenu/menu/migrations/0002_add_initial_data_to_menu_and_dish_model.py
+++ emenu/menu/migrations/0002_add_initial_data_to_menu_and_dish_model.py
@@ -7,28 +7,12 @@ class Migration(migrations.Migration):
     """Migration that creates initial data for Menu and Dish model"""
 
     dependencies = [
-        ('menu', '0002_alter_dish_options'),
+        ('menu', '0001_initial'),
     ]
 
     @staticmethod
     def generate_data(apps, schema_editor):
         """Generates initial data for Menu and Dish model"""
-        grande_restaurant_wro_menu = Menu(
-            name="Grande Restaurant Wroclaw Menu",
-            description="Menu for Grande Restaurant Wroclaw"
-        )
-        grande_restaurant_wro_menu.save()
-        grande_restaurant_krk_menu = Menu(
-            name="Grande Restaurant Krakow Menu",
-            description="Menu for Grande Restaurant Krakow"
-        )
-        grande_restaurant_krk_menu.save()
-        pizza_express_menu = Menu(
-            name="Pizza Express Menu",
-            description="Menu for Pizza Express"
-        )
-        pizza_express_menu.save()
-
         spaghetti = Dish(
             name="Spaghetti",
             description="Napoli spaghetti with tomatoes sauce and pork meat",
@@ -37,7 +21,6 @@ class Migration(migrations.Migration):
             is_vegetarian=False
         )
         spaghetti.save()
-        spaghetti.menus.add(grande_restaurant_wro_menu, grande_restaurant_krk_menu)
         spaghetti_wro = Dish(
             name="Wroclaw Chef Special Spaghetti",
             description="Wroclaw Chef Special Spaghetti",
@@ -46,7 +29,6 @@ class Migration(migrations.Migration):
             is_vegetarian=False
         )
         spaghetti_wro.save()
-        spaghetti_wro.menus.add(grande_restaurant_wro_menu)
         spaghetti_krk = Dish(
             name="Krakow Chef Special Spaghetti",
             description="Krakow Chef Special Spaghetti",
@@ -55,7 +37,6 @@ class Migration(migrations.Migration):
             is_vegetarian=True
         )
         spaghetti_krk.save()
-        spaghetti_krk.menus.add(grande_restaurant_krk_menu)
         margherita = Dish(
             name="Margherita",
             description="Cheese, tomatoes sauce, ham",
@@ -64,7 +45,6 @@ class Migration(migrations.Migration):
             is_vegetarian=False
         )
         margherita.save()
-        margherita.menus.add(pizza_express_menu)
         barbecue = Dish(
             name="Pizza Barbecue",
             description="Cheese, tomatoes sauce, ham, mushrooms, bbq sauce, chicken",
@@ -73,7 +53,25 @@ class Migration(migrations.Migration):
             is_vegetarian=False
         )
         barbecue.save()
-        barbecue.menus.add(pizza_express_menu)
+
+        grande_restaurant_wro_menu = Menu(
+            name="Grande Restaurant Wroclaw Menu",
+            description="Menu for Grande Restaurant Wroclaw"
+        )
+        grande_restaurant_wro_menu.save()
+        grande_restaurant_wro_menu.dishes.add(spaghetti, spaghetti_wro)
+        grande_restaurant_krk_menu = Menu(
+            name="Grande Restaurant Krakow Menu",
+            description="Menu for Grande Restaurant Krakow"
+        )
+        grande_restaurant_krk_menu.save()
+        grande_restaurant_krk_menu.dishes.add(spaghetti, spaghetti_krk)
+        pizza_express_menu = Menu(
+            name="Pizza Express Menu",
+            description="Menu for Pizza Express"
+        )
+        pizza_express_menu.save()
+        pizza_express_menu.dishes.add(margherita, barbecue)
 
     operations = [
         migrations.RunPython(generate_data),
--- emenu/menu/models.py
+++ emenu/menu/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 
 
-class Menu(models.Model):
-    """Menu model"""
-    name = models.fields.CharField(max_length=32, unique=True)
-    description = models.fields.CharField(max_length=512)
-    created = models.fields.DateTimeField(auto_now_add=True)
-    updated = models.fields.DateTimeField(auto_now=True)
-
-    def __str__(self) -> str:
-        """__str__"""
-        return str(self.name)
-
-
 class Dish(models.Model):
     """Dish model"""
     name = models.fields.CharField(max_length=32)
@@ -22,11 +10,23 @@ class Dish(models.Model):
     created = models.fields.DateTimeField(auto_now_add=True)
     updated = models.fields.DateTimeField(auto_now=True)
     is_vegetarian = models.fields.BooleanField()
-    menus = models.ManyToManyField(Menu, related_name='dishes')
 
     class Meta:
         """Meta info for dish model"""
         verbose_name_plural = "Dishes"
+
+    def __str__(self) -> str:
+        """__str__"""
+        return str(self.name)
+
+
+class Menu(models.Model):
+    """Menu model"""
+    name = models.fields.CharField(max_length=32, unique=True)
+    description = models.fields.CharField(max_length=512)
+    created = models.fields.DateTimeField(auto_now_add=True)
+    updated = models.fields.DateTimeField(auto_now=True)
+    dishes = models.ManyToManyField(Dish, related_name='menus')
 
     def __str__(self) -> str:
         """__str__"""
